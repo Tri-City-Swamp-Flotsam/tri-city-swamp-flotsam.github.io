52dcd87a9c183b2775cb4c14fe1961f2da09747e
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-04-02 19:42:12 +0000

    let

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"];
 var current_frame = 0;
 
-const setTitle = async () => {
+let setTitle = async () => {
     if (current_frame > 7){current_frame = 0;}
 
     document.title = frames[current_frame];

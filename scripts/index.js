11beae0268c4c2bf36977180f32206f5269f7154
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-04-02 19:43:58 +0000

    debug

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,12 @@
 const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"];
 var current_frame = 0;
 
-let setTitle = async () => {
+const setTitle = async () => {
     if (current_frame > 7){current_frame = 0;}
 
+    document.body = toString(current_frame)
     document.title = frames[current_frame];
+    document.body = toString(current_frame)
     current_frame++;
     await sleep(.5);
 }

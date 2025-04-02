5cf5147675effb78789b7f8772f5c9dddde49f80
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-04-02 19:40:24 +0000

    changed some things

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 // Animate document title
 const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"];
-let current_frame = 0;
+var current_frame = 0;
 
 const setTitle = async () => {
+    if (current_frame > 7){current_frame = 0;}
+
     document.title = frames[current_frame];
     current_frame++;
     await sleep(.5);
-
-    if (current_frame > frames.length){current_frame = 0;}
 }
 
 setInterval(setTitle, 500);

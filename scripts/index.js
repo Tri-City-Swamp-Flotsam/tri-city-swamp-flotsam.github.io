f48b88ef2107d1702506441e0c4615bb6c5e4443
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-04-02 18:43:17 +0000

    add wait time

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,12 @@
 const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"]
 let current_frame = 0
 
-while (true) {
+function setTitle(){
     document.title = frames[current_frame]
+}
+
+while (true) {
+    setTimeout(setTitle,1000*0.5)
     current_frame++
 
     if (current_frame > frames.length){

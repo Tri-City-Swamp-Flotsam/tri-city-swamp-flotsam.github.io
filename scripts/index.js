c03f289307f0d3d9c5e226dfd08fe03c6d1eef38
Richard Scott <82393276+Bububooo3@users.noreply.github.com>
2025-04-02 19:36:29 +0000

    interval loop

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,13 @@
 // Animate document title
-const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"]
-let current_frame = 0
-
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay*1000))
-// ex. await sleep(seconds)
+const frames = ["T-c-s-f","t-C-s-f","t-c-S-f","t-c-s-F","t-c-s-f","t-c-s-f","t-c-s-f"];
+let current_frame = 0;
 
 const setTitle = async () => {
-    document.title = frames[current_frame]
-    current_frame++
-    await sleep(.5)
+    document.title = frames[current_frame];
+    current_frame++;
+    await sleep(.5);
 
-    if (current_frame > frames.length){current_frame = 0}
+    if (current_frame > frames.length){current_frame = 0;}
 }
 
-while (true) {
-    setTitle()
-}
+setInterval(setTitle, 500);
